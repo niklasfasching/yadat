26b4155253d2a98a456cd42f4d5531e53ed88cf3
Niklas Fasching <niklas.fasching@gmail.com>
2018-04-23 22:51:32 +0200

    init

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject yadat "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :target-path "target/%s")
---
+++ test/yadat/core_test.clj
@@ -0,0 +1,3 @@
+(ns yadat.core-test
+  (:require [clojure.test :refer :all]
+            [yadat.core :as core]))
